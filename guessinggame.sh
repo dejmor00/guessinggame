a34354850e07cfb84f70c615ef710cae1b994bae
dejmor00 <dejan.morar@gmail.com>
2020-05-02 14:56:19 +0200

    while loop added

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,11 +1,36 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-# Check the number of files in a folder
+
+# Check the number of files in the current directory
 counter=0
 let counter=$(ls -l | tail -n +2 | wc -l)
 
 # Ask user to enter the number of files
-echo "How many files are in this directory?"
-echo "Your guess?"
-read guess
+echo "How many files are in the current directory?"
+
+# Run script
+run=1
+while [[ $run -eq 1 ]]
+do
+    echo "Your guess:"
+    read guess
+
+    if [[ $guess -lt $counter ]]
+    then
+    	echo "Wrong! There are more files, try again."
+    elif [[ $guess -gt $counter ]]
+    then
+    	echo "Wrong! There are less files, try again."
+    elif [[ $guess -eq $counter ]]
+    then
+    	echo "Congratulations, your answer is correct!"
+	let run=0
+    fi
+done
+
+
+
+
+# while
+

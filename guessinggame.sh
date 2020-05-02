15926a414ff2e5564f3037ac0e9de62df360b8f2
dejmor00 <dejan.morar@gmail.com>
2020-05-02 13:33:53 +0200

    counter added

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-# Ask user to enter the number of files
+# Check the number of files in a folder
+counter=0
+let counter=$(ls -l | tail -n +2 | wc -l)
 
+# Ask user to enter the number of files
 echo "How many files are in this directory?"
 echo "Your guess?"
 read guess
